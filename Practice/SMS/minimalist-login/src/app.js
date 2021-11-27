fe6d13caf266e08f364c874ce7adef452ab50196
Arindam <arindamkeswani@gmail.com>
2021-11-27 15:21:34 +0530

    Redirection failed

--- Practice/SMS/minimalist-login/src/app.js
+++ Practice/SMS/minimalist-login/src/app.js
@@ -11,6 +11,7 @@ app.use(cors())
 app.use(express.static('view'));
 
 app.use(express.json());
+// app.use(express.urlencoded)
 
 // app.listen(3000);
 const port=process.env.PORT || 5000;
@@ -19,10 +20,18 @@ app.listen(port, function(){
 })
 // app.use(cookieParser());
 
+app.set('view engine', 'html');
+
+app.engine('html', require('ejs').renderFile);
+
 app.get('/', (req, res)=>{
     res.sendFile('E:\\Snippets\\Backend\\Twilio\\Practice\\SMS\\minimalist-login\\src\\view\\index.html');
 }) 
 
+
+app.use('/homePage', (req, res)=>{
+    res.sendFile('E:\\Snippets\\Backend\\Twilio\\Practice\\SMS\\minimalist-login\\src\\view\\home.html');
+}) ;
 const userRouter = require('./router/userRouter');
 app.use("/user", userRouter);
 
--- Practice/SMS/minimalist-login/src/controller/loginScript.js
+++ Practice/SMS/minimalist-login/src/controller/loginScript.js
@@ -1,24 +1,43 @@
 const express = require('express');
+const app = express();
 const authRouter = express.Router();
-// const generatedOTP = require('./generateOTP')
+const generatedOTP = require('./generateOTP')
 const sendOTPsms = require('./sendOTP').sendOTP
 // const userModel = require('../models/userModels');
 //ywgdyqws
 
-module.exports.login =function loginUser(req, res) {
+module.exports.login = async function loginUser(req, res) {
+
+    //MANUAL 
+    const otp = req.body.otp
+    console.log(req.body.otp, generatedOTP);
+    if (otp == generatedOTP) {
+        // res.send("true");
+        // console.log('hi loginscript');
+        return await res.redirect('/homePage');
+            // //console.log(req, res);
+            // return res.sendFile('Practice\\SMS\\minimalist-login\\src\\view\\home.html', 
+            // {root:"E:\\Snippets\\Backend\\Twilio"});
+        
+    }
+    else {
+        console.log("Wrong");
+    }
+    //VERIFY API
     // try {
-        
-        const accountSid = process.env.ACCOUNT_SID;
-        const authToken = process.env.AUTH_TOKEN;
-        const client = require('twilio')(accountSid, authToken);
-        const otp = req.body.otp
-        const phone = req.body.phone
-        // console.log(phone, otp);
-        client.verify.services(process.env.SERVICE_SID)
-            .verificationChecks
-            .create({to: phone, code: otp})
-            .then(verification_check => console.log(verification_check.status));
-        
+    // //----------------------------------------------------------------------
+    // const accountSid = process.env.ACCOUNT_SID;
+    // const authToken = process.env.AUTH_TOKEN;
+    // const client = require('twilio')(accountSid, authToken);
+    // const otp = req.body.otp
+    // const phone = req.body.phone
+    // // console.log(phone, otp);
+    // client.verify.services(process.env.SERVICE_SID)
+    //     .verificationChecks
+    //     .create({to: phone, code: otp})
+    //     .then(verification_check => console.log(verification_check.status))
+    //     .then(res.redirect('/home'));
+    //-----------------------------------------------------------------------
     // }catch (err) {
     //     res.statusCode = 500
     //     return res.json({
